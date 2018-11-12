2ac8387abfe04d3f1502a405dfb8c1793b8f2021
Ricardo Boluda <rboluda@geoblink.com>
2018-11-12 11:48:19 +0100

    Fix tests for fromPairsMapNonNil

--- test/fromPairsMapNonNil.spec.js
+++ test/fromPairsMapNonNil.spec.js
@@ -11,12 +11,16 @@ describe('fromPairsMapNonNil', function () {
       a: 'a',
       b: 'b',
     }
-    const result = fromPairsMapNonNil(array, v => [v, v])
+    const result = fromPairsMapNonNil(array, (v) => {
+      return !!v ? [v, v] : null
+    })
     expect(result).to.be.deep.equal(expectedResult)
   })
 
   it('should apply map function', function () {
-    const mapFunction = sinon.stub().callsFake(v => [v, `the key is ${v}`])
+    const mapFunction = sinon.stub().callsFake((v) => {
+      return !!v ? [v, `the key is ${v}`] : null
+    })
     const array = ['a', null, 'c']
     const expectedResult = {
       a: 'the key is a',
@@ -25,7 +29,7 @@ describe('fromPairsMapNonNil', function () {
     const result = fromPairsMapNonNil(array, mapFunction)
     expect(mapFunction).to.have.property('callCount').that.is.equal(array.length)
     expect(mapFunction).to.have.been.calledWith('a', 0)
-    expect(mapFunction).to.have.been.calledWith('b', 1)
+    expect(mapFunction).to.have.been.calledWith(null, 1)
     expect(mapFunction).to.have.been.calledWith('c', 2)
     expect(result).to.be.deep.equal(expectedResult)
   })
