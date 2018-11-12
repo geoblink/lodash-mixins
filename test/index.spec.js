44fc82b349d99a1c109e28705facbf85070b141a
Ricardo Boluda <rboluda@geoblink.com>
2018-11-12 13:29:32 +0100

    Refactor index test

--- test/index.spec.js
+++ test/index.spec.js
@@ -2,27 +2,19 @@ const chai = require('chai')
 const { expect } = chai
 chai.use(require('sinon-chai'))
 const _ = require('lodash')
-const fs = require('fs')
+const fs = require('fs-extra')
 const path = require('path')
 require('../src')(_)
 
 describe('Index imports', function () {
-  let lodashMixinsFuncs
-  before(function () {
-    const notLodashMixinsFiles = ['index.js', 'constants.js']
-    fs.readdir('src', function (err, files) {
-      if (err) {
-        return false
-      }
-      var lodashMixinsFiles = _.reject(files, function (f) {
-        return _.includes(notLodashMixinsFiles, f)
-      })
-      lodashMixinsFuncs = _.map(lodashMixinsFiles, function (f) {
-        return path.basename(f, path.extname(f))
-      })
-    })
-  })
-  it('There should be a lodash function for each src file', function () {
+  it('There should be a lodash function for each src file', async function () {
+    const notLodashMixinsFiles = {
+      'index.js': true,
+      'constants.js': true
+    }
+    const files = await fs.readdir('src')
+    const lodashMixinsFiles = _.reject(files, f => f in notLodashMixinsFiles)
+    const lodashMixinsFuncs = _.map(lodashMixinsFiles, f => path.basename(f, path.extname(f)))
     _.forEach(lodashMixinsFuncs, function (f) {
       expect(_).to.have.property(f).that.is.a('function')
     })
