4317bee2f7ebf9057e73bd40a53a3ed6308aef52
Lluís Ulzurrun de Asanza Sàez <me@llu.is>
2018-11-12 12:04:02 +0100

    Use code brackets for nil keyword
    Co-Authored-By: riboher <riboher@gmail.com>

--- src/fromPairsMapNonNil.js
+++ src/fromPairsMapNonNil.js
@@ -3,7 +3,7 @@ const mapNonNil = require('../src/mapNonNil')
 module.exports = fromPairsMapNonNil
 
 /**
- * Applies `fromPairs` to the result of mapping and filtering nil values given `iteratee` to given
+ * Applies `fromPairs` to the result of mapping and filtering `nil` values given `iteratee` to given
  * collection.
  *
  * @param {Array|Object} collection Collection to iterate over
