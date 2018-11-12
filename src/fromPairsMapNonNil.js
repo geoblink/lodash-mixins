e969f45019b071decb2ceee0ac65cea1f1d40883
Ricardo Boluda <rboluda@geoblink.com>
2018-11-12 11:40:02 +0100

    :construction: Add fromPairsMapNonNil function files

---
+++ src/fromPairsMapNonNil.js
@@ -0,0 +1,15 @@
+var _ = require('lodash')
+const mapNonNil = require('../src/mapNonNil')
+module.exports = fromPairsMapNonNil
+
+/**
+ * Applies `fromPairs` to the result of mapping and filtering nil values given `iteratee` to given
+ * collection.
+ *
+ * @param {Array|Object} collection Collection to iterate over
+ * @param {Function} iteratee Function invoked per iteration
+ * @returns {Object} New object
+ */
+function fromPairsMapNonNil (collection, iteratee) {
+  return _.fromPairs(mapNonNil(collection, iteratee))
+}
