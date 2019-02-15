ea68d41c52d2dc9bdb36bfe7b84a34b1837c0499
Lluís Ulzurrun de Asanza i Sàez <lulzurrun@geoblink.com>
2019-02-15 15:41:32 +0100

    :white_check_mark: Add `shortcuttedReduce` Lodash types

--- src/shortcuttedReduce.ts
+++ src/shortcuttedReduce.ts
@@ -71,3 +71,170 @@ function shortcuttedReduce<
     return accumulator
   }
 }
+
+declare module 'lodash' {
+  interface LoDashStatic {
+    /**
+     * Reduce which finishes as soon as accumulator changes.
+     *
+     * @param collection Collection to be reduced.
+     * @param iteratee Function that will be called in each iteration.
+     * It'll receive as parameters the accumulator, the value and the index.
+     * @param accumulator Note that is **must** be a simple primitive.
+     * @returns Accumulated value.
+     */
+    shortcuttedReduce<T, TResult>(
+      collection: T[] | null | undefined,
+      callback: MemoListIterator<T, TResult, T[]>,
+      accumulator: TResult
+    ): TResult;
+
+    /**
+     * @see _.shortcuttedReduce
+     */
+    shortcuttedReduce<T, TResult>(
+      collection: List<T> | null | undefined,
+      callback: MemoListIterator<T, TResult, List<T>>,
+      accumulator: TResult
+    ): TResult;
+
+    /**
+     * @see _.shortcuttedReduce
+     */
+    shortcuttedReduce<T extends object, TResult>(
+      collection: T | null | undefined,
+      callback: MemoObjectIterator<T[keyof T], TResult, T>,
+      accumulator: TResult
+    ): TResult;
+
+    /**
+     * @see _.shortcuttedReduce
+     */
+    shortcuttedReduce<T>(
+      collection: T[] | null | undefined,
+      callback: MemoListIterator<T, T, T[]>
+    ): T | undefined;
+
+    /**
+     * @see _.shortcuttedReduce
+     */
+    shortcuttedReduce<T>(
+      collection: List<T> | null | undefined,
+      callback: MemoListIterator<T, T, List<T>>
+    ): T | undefined;
+
+    /**
+     * @see _.shortcuttedReduce
+     */
+    shortcuttedReduce<T extends object>(
+      collection: T | null | undefined,
+      callback: MemoObjectIterator<T[keyof T], T[keyof T], T>
+    ): T[keyof T] | undefined;
+  }
+
+  interface LoDashImplicitWrapper<TValue> {
+    /**
+     * @see _.shortcuttedReduce
+     */
+    shortcuttedReduce<T, TResult>(
+      this: LoDashImplicitWrapper<T[] | null | undefined>,
+      callback: MemoListIterator<T, TResult, T[]>,
+      accumulator: TResult
+    ): TResult;
+
+    /**
+     * @see _.shortcuttedReduce
+     */
+    shortcuttedReduce<T, TResult>(
+      this: LoDashImplicitWrapper<List<T> | null | undefined>,
+      callback: MemoListIterator<T, TResult, List<T>>,
+      accumulator: TResult
+    ): TResult;
+
+    /**
+    * @see _.shortcuttedReduce
+    */
+    shortcuttedReduce<T extends object, TResult>(
+      this: LoDashImplicitWrapper<T | null | undefined>,
+      callback: MemoObjectIterator<T[keyof T], TResult, T>,
+      accumulator: TResult
+    ): TResult;
+
+    /**
+    * @see _.shortcuttedReduce
+    */
+    shortcuttedReduce<T>(
+      this: LoDashImplicitWrapper<T[] | null | undefined>,
+      callback: MemoListIterator<T, T, T[]>
+    ): T | undefined;
+
+    /**
+    * @see _.shortcuttedReduce
+    */
+    shortcuttedReduce<T>(
+      this: LoDashImplicitWrapper<List<T> | null | undefined>,
+      callback: MemoListIterator<T, T, List<T>>
+    ): T | undefined;
+
+    /**
+    * @see _.shortcuttedReduce
+    */
+    shortcuttedReduce<T extends object>(
+      this: LoDashImplicitWrapper<T | null | undefined>,
+      callback: MemoObjectIterator<T[keyof T], T[keyof T], T>
+    ): T[keyof T] | undefined;
+  }
+
+  interface LoDashExplicitWrapper<TValue> {
+    /**
+    * @see _.shortcuttedReduce
+    */
+    shortcuttedReduce<T, TResult>(
+      this: LoDashExplicitWrapper<T[] | null | undefined>,
+      callback: MemoListIterator<T, TResult, T[]>,
+      accumulator: TResult
+    ): LoDashExplicitWrapper<TResult>;
+
+    /**
+    * @see _.shortcuttedReduce
+    */
+    shortcuttedReduce<T, TResult>(
+      this: LoDashExplicitWrapper<List<T> | null | undefined>,
+      callback: MemoListIterator<T, TResult, List<T>>,
+      accumulator: TResult
+    ): LoDashExplicitWrapper<TResult>;
+
+    /**
+    * @see _.shortcuttedReduce
+    */
+    shortcuttedReduce<T extends object, TResult>(
+      this: LoDashExplicitWrapper<T | null | undefined>,
+      callback: MemoObjectIterator<T[keyof T], TResult, T>,
+      accumulator: TResult
+    ): LoDashExplicitWrapper<TResult>;
+
+    /**
+    * @see _.shortcuttedReduce
+    */
+    shortcuttedReduce<T>(
+      this: LoDashExplicitWrapper<T[] | null | undefined>,
+      callback: MemoListIterator<T, T, T[]>
+    ): LoDashExplicitWrapper<T | undefined>;
+
+    /**
+    * @see _.shortcuttedReduce
+    */
+    shortcuttedReduce<T>(
+      this: LoDashExplicitWrapper<List<T> | null | undefined>,
+      callback: MemoListIterator<T, T, List<T>>
+    ): LoDashExplicitWrapper<T | undefined>;
+
+    /**
+    * @see _.shortcuttedReduce
+    */
+    shortcuttedReduce<T extends object>(
+      this: LoDashExplicitWrapper<T | null | undefined>,
+      callback: MemoObjectIterator<T[keyof T], T[keyof T], T>
+    ): LoDashExplicitWrapper<T[keyof T] | undefined>;
+  }
+}
