1afd7b6d51a7936a303db38f24bf6c8372bfbbe3
Jose Luis Pillado <jlpillado@geoblink.com>
2021-02-08 13:54:04 +0100

    :bulb: Update getTruthyKeys documentation

--- src/getTruthyKeys.ts
+++ src/getTruthyKeys.ts
@@ -8,10 +8,8 @@ export default getTruthyKeys
  * @param collection Collection to iterate over.
  * @param parseKeyFunction Function invoked per iteration. This
  * function takes as parameter a key of a truthy value. The value it returns
- * will be used in resulting list. Note that if this function returns a falsy
- * value value, the key won't be present in resulting collection.
- * @returns New list with just the keys of truthy entries, for which
- * `parseKeyFunction` returned a truthy value.
+ * will be used in resulting list.
+ * @returns New list with just the keys of truthy entries.
  */
 function getTruthyKeys<T extends object, TResult>(
   collection: T | null | undefined,
