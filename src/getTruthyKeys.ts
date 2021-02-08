9e475624ac47a9f62799d799f3257b3b8682379c
Jose Luis Pillado <jlpillado@geoblink.com>
2021-02-08 13:54:29 +0100

    :bulb: Update getTruthyKeys documentation

--- src/getTruthyKeys.ts
+++ src/getTruthyKeys.ts
@@ -34,10 +34,8 @@ declare module 'lodash' {
      * @param collection Collection to iterate over.
      * @param parseKeyFunction Function invoked per iteration. This
      * function takes as parameter a key of a truthy value. The value it returns
-     * will be used in resulting list. Note that if this function returns a falsy
-     * value value, the key won't be present in resulting collection.
-     * @returns New list with just the keys of truthy entries, for which
-     * `parseKeyFunction` returned a truthy value.
+     * will be used in resulting list.
+     * @returns New list with just the keys of truthy entries.
      */
     getTruthyKeys<T extends object, TResult>(
       collection: T | null | undefined,
